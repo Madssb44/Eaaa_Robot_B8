a10c27521a3f2c78770c30d658660205eb282f9a
Mads <madssb@gmail.com>
2026-01-28 20:30:29 +0100

    started working on classes

--- src/movement/motor_movement.py
+++ src/movement/motor_movement.py
@@ -6,18 +6,18 @@ class Motor:
     MIN_DUTY = 15000
     MAX_DUTY = 65636
 
-    def __init__(self, pin1, pin2, PWM_pin):
+    def __init__(self, pin1, pin2, PWM_pin, speed=0):
         self.pin1 = Pin(pin1, Pin.OUT) 
         self.pin2 = Pin(pin2, Pin.OUT)
-        self.PWM_pin = PWM(Pin(PWM_pin)
-        self.duty = duty_u16()
+        self.PWM_pin = PWM(Pin(PWM_pin))
+        #self.duty = duty_u16(0)
         
         self.speed = 0
 
     def go_forward(self, speed):
         self.pin1.on()
         self.pin2.off()
-        self.
+        self.PWM_pin.freq(duty_u16()
 
     def go_back(self, speed):
 
@@ -27,4 +27,8 @@ class Motor:
 
     def turn_left(self):
 
-
+    def calc_speed(self, speed):
+        if speed <= 0 or speed > 100:
+            duty_cycle = 0
+        else:
+            duty_cycle = duty.speed()
